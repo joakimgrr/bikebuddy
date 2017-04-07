d6d33dab59889794cae51e1c855b9ee89eaffccf
Joakim Granbohm <joakim.granbohm@sysart.fi>
2017-04-07 14:58:11 +0300

    add react native maps with navigation

--- android/app/src/main/java/com/react_native_navigation_bootstrap/MainApplication.java
+++ android/app/src/main/java/com/react_native_navigation_bootstrap/MainApplication.java
@@ -4,8 +4,10 @@ import android.support.annotation.NonNull;
 
 import com.facebook.react.ReactPackage;
 import com.reactnativenavigation.NavigationApplication;
+import com.airbnb.android.react.maps.MapsPackage;
 
 import java.util.List;
+import java.util.Arrays;
 
 public class MainApplication extends NavigationApplication {
 
@@ -17,9 +19,12 @@ public class MainApplication extends NavigationApplication {
   @NonNull
   @Override
   public List<ReactPackage> createAdditionalReactPackages() {
+    return Arrays.<ReactPackage>asList(
+      new MapsPackage()
+    );
     // Add the packages you require here.
     // No need to add RnnPackage and MainReactPackage
-    return null;
+    //return null;
   }
 
 }
